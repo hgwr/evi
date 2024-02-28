20a285f084c828a0df97816db2ac1398f666f6a6
Shigeru Hagiwara <hgwrsgr@gmail.com>
2024-02-28 21:22:03 +0900

    debugging cursor move

--- src/command/commands/move_cursor.rs
+++ src/command/commands/move_cursor.rs
@@ -5,8 +5,8 @@ use unicode_width::UnicodeWidthChar;
 use crate::command::base::Command;
 use crate::editor::Editor;
 
-pub struct RightOneChar;
-impl Command for RightOneChar {
+pub struct ForwardChar;
+impl Command for ForwardChar {
     fn execute(&mut self, editor: &mut Editor) {
         let line = &editor.buffer.lines[editor.cursor_position_in_buffer.row];
         let num_of_chars = line.chars().count();
@@ -32,8 +32,8 @@ impl Command for RightOneChar {
     }
 }
 
-pub struct LeftOneChar;
-impl Command for LeftOneChar {
+pub struct BackwardChar;
+impl Command for BackwardChar {
     fn execute(&mut self, editor: &mut Editor) {
         if editor.cursor_position_in_buffer.col > 0 {
             editor.cursor_position_in_buffer.col -= 1;
@@ -58,8 +58,8 @@ impl Command for LeftOneChar {
     }
 }
 
-pub struct DownOneLine;
-impl Command for DownOneLine {
+pub struct NextLine;
+impl Command for NextLine {
     fn execute(&mut self, editor: &mut Editor) {
         let next_row = editor.cursor_position_in_buffer.row + 1;
         if next_row < editor.buffer.lines.len() {
@@ -84,8 +84,8 @@ impl Command for DownOneLine {
     }
 }
 
-pub struct UpOneLine;
-impl Command for UpOneLine {
+pub struct PreviousLine;
+impl Command for PreviousLine {
     fn execute(&mut self, editor: &mut Editor) {
         if editor.cursor_position_in_buffer.row > 0 {
             editor.cursor_position_in_buffer.row -= 1;
--- src/command/factory.rs
+++ src/command/factory.rs
@@ -1,15 +1,15 @@
 use crate::command::base::{Command, CommandData, JumpCommandData};
 use crate::command::commands::exit::ExitCommand;
-use crate::command::commands::move_cursor::{DownOneLine, LeftOneChar, RightOneChar, UpOneLine};
+use crate::command::commands::move_cursor::{NextLine, BackwardChar, ForwardChar, PreviousLine};
 use crate::command::commands::no_op_command::NoOpCommand;
 use crossterm::event::KeyCode;
 
 pub fn command_factory(command_data: &CommandData) -> Box<dyn Command> {
     match command_data.command {
-        KeyCode::Char('j') | KeyCode::Down => Box::new(DownOneLine {}),
-        KeyCode::Char('k') | KeyCode::Up => Box::new(UpOneLine {}),
-        KeyCode::Char('l') | KeyCode::Right => Box::new(RightOneChar {}),
-        KeyCode::Char('h') | KeyCode::Left => Box::new(LeftOneChar {}),
+        KeyCode::Char('j') | KeyCode::Down => Box::new(NextLine {}),
+        KeyCode::Char('k') | KeyCode::Up => Box::new(PreviousLine {}),
+        KeyCode::Char('l') | KeyCode::Right => Box::new(ForwardChar {}),
+        KeyCode::Char('h') | KeyCode::Left => Box::new(BackwardChar {}),
         KeyCode::Char('Z') => {
             if let Some(JumpCommandData { count, command }) = command_data.range {
                 if count == 1 && command == KeyCode::Char('Z') {

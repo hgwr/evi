96139b19b05160d4fc3c8f6a1d5911ce184bbc91
Shigeru Hagiwara <hgwrsgr@gmail.com>
2024-05-26 11:22:13 +0900

    不具合修正の方針について

--- src/command/commands/delete.rs
+++ src/command/commands/delete.rs
@@ -118,12 +118,12 @@ impl Command for Delete {
         if let Some(jump_command_data) = self.jump_command_data_opt {
             // TODO jump_command_data が行指向の場合は行全体を範囲指定に使うように修正する
             // TODO jump_command_data が行末の場合は範囲指定で行末まで削除するように修正する
+            let start_cursor_data = editor.snapshot_cursor_data();
             let command_data: crate::command::base::CommandData = jump_command_data.into();
             for _ in 0..command_data.count {
                 let mut jump_command = command_factory(&command_data);
                 jump_command.execute(editor)?;
             }
-            let start_cursor_data = editor.snapshot_cursor_data();
             let end_cursor_data = editor.snapshot_cursor_data();
             if let Ok(deleted) = editor.buffer.delete(
                 start_cursor_data.cursor_position_in_buffer,

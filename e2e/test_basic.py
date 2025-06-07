53f4dc6ece3b1d88bb993363ec597cf3760dda4b
Shigeru Hagiwara <hgwrsgr@gmail.com>
2025-06-07 13:58:41 +0900

    Remove Python cache files

---
+++ e2e/test_basic.py
@@ -0,0 +1,17 @@
+from .helpers import run_commands
+
+def test_insert_and_save():
+    result = run_commands(['i', 'Hello, Evi!', '\x1b'])
+    assert 'Hello, Evi!' in result
+
+def test_open_new_line():
+    result = run_commands(['o', 'second line', '\x1b'], initial_content='first\n')
+    assert result.splitlines() == ['first', 'second line']
+
+def test_delete_line():
+    result = run_commands(['j', 'dd'], initial_content='a\nb\nc\n')
+    assert result.splitlines() == ['a', 'c']
+
+def test_substitute():
+    result = run_commands([':s/foo/bar/\r', 'j', ':s/foo/bar/g\r'], initial_content='foo\nfoo foo\n')
+    assert result.splitlines() == ['bar', 'bar bar']

66a2f477130074d66c1240e235f3ca2d4f2f4643
Shigeru Hagiwara <hgwrsgr@gmail.com>
2025-06-08 05:43:20 +0900

    Add undo and repeat e2e tests

--- e2e/test_vi_commands.py
+++ e2e/test_vi_commands.py
@@ -6,9 +6,29 @@ def test_append_after_cursor():
     assert result.strip() == 'abc'
 
 
+def test_append_after_cursor_undo():
+    result = run_commands(['a', 'b', '\x1b', 'u'], initial_content='ac\n')
+    assert result.strip() == 'ac'
+
+
+def test_append_after_cursor_repeat():
+    result = run_commands(['a', 'b', '\x1b', '.', '\x1b'], initial_content='ac\n')
+    assert result.strip() == 'abbc'
+
+
 def test_open_line_above():
     result = run_commands(['O', 'first', '\x1b'], initial_content='second\n')
     assert result.splitlines() == ['first', 'second']
+
+
+def test_open_line_above_undo():
+    result = run_commands(['O', 'first', '\x1b', 'u'], initial_content='second\n')
+    assert result.strip() == 'second'
+
+
+def test_open_line_above_repeat():
+    result = run_commands(['O', 'first', '\x1b', '.', '\x1b'], initial_content='second\n')
+    assert result.splitlines() == ['first', 'first', 'second']
 
 
 def test_delete_char():
@@ -21,14 +41,39 @@ def test_delete_word():
     assert result.strip() == 'bar'
 
 
+def test_delete_word_undo():
+    result = run_commands(['d', 'w', 'u'], initial_content='foo bar\n')
+    assert result.strip() == 'foo bar'
+
+
 def test_search_forward_delete_line():
     result = run_commands(['/bar\r', 'dd'], initial_content='foo\nbar\nbaz\n')
     assert result.splitlines() == ['foo', 'baz']
 
 
+def test_search_forward_delete_line_undo():
+    result = run_commands(['/bar\r', 'dd', 'u'], initial_content='foo\nbar\nbaz\n')
+    assert result.splitlines() == ['foo', 'bar', 'baz']
+
+
+def test_search_forward_delete_line_repeat():
+    result = run_commands(['/bar\r', 'dd', '.'], initial_content='foo\nbar\nbaz\n')
+    assert result.splitlines() == ['foo']
+
+
 def test_search_backward_delete_line():
     result = run_commands(['j', '?foo\r', 'dd'], initial_content='foo\nbar\nfoo\n')
     assert result.splitlines() == ['bar', 'foo']
+
+
+def test_search_backward_delete_line_undo():
+    result = run_commands(['j', '?foo\r', 'dd', 'u'], initial_content='foo\nbar\nfoo\n')
+    assert result.splitlines() == ['foo', 'bar', 'foo']
+
+
+def test_search_backward_delete_line_repeat():
+    result = run_commands(['j', '?foo\r', 'dd', '.'], initial_content='foo\nbar\nfoo\n')
+    assert result.splitlines() == ['foo']
 
 
 def test_undo():

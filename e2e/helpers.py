8609ff86972002e744b53165ac4141c5cdae999b
Shigeru Hagiwara <hgwrsgr@gmail.com>
2025-06-07 15:17:11 +0900

    Use try-finally for temp file cleanup

--- e2e/helpers.py
+++ e2e/helpers.py
@@ -6,17 +6,19 @@ from .conftest import EVI_BIN
 
 def run_commands(commands, initial_content=""):
     fd, path = tempfile.mkstemp()
-    with os.fdopen(fd, 'w') as f:
-        f.write(initial_content)
-    env = os.environ.copy()
-    env.setdefault('TERM', 'xterm')
-    child = pexpect.spawn(EVI_BIN, [path], env=env, encoding='utf-8')
-    child.delaybeforesend = 0.05
-    for c in commands:
-        child.send(c)
-    child.send(':wq\r')
-    child.expect(pexpect.EOF)
-    with open(path) as f:
-        result = f.read()
-    os.unlink(path)
+    try:
+        with os.fdopen(fd, 'w') as f:
+            f.write(initial_content)
+        env = os.environ.copy()
+        env.setdefault('TERM', 'xterm')
+        child = pexpect.spawn(EVI_BIN, [path], env=env, encoding='utf-8')
+        child.delaybeforesend = 0.05
+        for c in commands:
+            child.send(c)
+        child.send(':wq\r')
+        child.expect(pexpect.EOF)
+        with open(path) as f:
+            result = f.read()
+    finally:
+        os.unlink(path)
     return result
